48cb8a4773b7b6a990bb043235118b42e0f727dd
Chaitanya <38726636+chaitanya-jandhyam@users.noreply.github.com>
2019-11-15 12:29:29 +0530

    Update Add.py

--- Add.py
+++ Add.py
@@ -1,6 +1,6 @@
 def add_numbers(x,y):
-sum = x + y
-return sum
-num1 =input("enter the numbers)
+    sum = x + y
+    return sum
+num1 =input("enter the numbers")
 num2 = input("enter the number")
 print("The sum is", add_numbers(num1, num2))
