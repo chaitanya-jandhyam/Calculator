b37ccdec9382b6d40ba9797e792f1344706cc59c
Chaitanya <38726636+chaitanya-jandhyam@users.noreply.github.com>
2019-11-15 13:00:57 +0530

    Update Subtraction.py

--- Subtraction.py
+++ Subtraction.py
@@ -1,6 +1,6 @@
-num1 = input('Enter first number: ')
-num2 = input('Enter second number: ')
+class sub():
+ num1 = int(input('Enter first number: '))
+ num2 = int(input('Enter second number: '))
+ return(int(num1-num2))
  
-sub = float(num1) - float(num2)
- 
-print('The subtraction of {0} and {1} is: {2}'.format(num1, num2, sum))
+
