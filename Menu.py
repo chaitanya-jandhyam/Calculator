dc50c9ab017b24b9f67f81d876d1fac97586c31b
Chaitanya <38726636+chaitanya-jandhyam@users.noreply.github.com>
2019-11-15 13:04:31 +0530

    Update Menu.py

--- Menu.py
+++ Menu.py
@@ -4,5 +4,14 @@ class Menu():
 	print("2. Multiplication")
 	print("3. Subraction")
 	print("4. Division")
-  
-  if A==1
+ if A==1:
+		   from Add.py import add()
+ if A==2:
+		   from Multiply.py import multiply()
+ if A==3:
+		   from Subraction.py import sub()
+ if A==4:
+		   from Division.py import division()
+	
+
+		   
